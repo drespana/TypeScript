84347c772b2eb8866004d89435ed907e5594b1cb
drespana <drespana@google.com>
2023-11-07 12:41:16 -0600

    notes

---
+++ test1.ts
@@ -0,0 +1,12 @@
+
+enum Color {
+    Red = "Red",
+    Orange = "Orange",
+    Yellow = "Yellow",
+    Green = "Green",
+    Blue = "Blue",
+    Purple = "Purple",
+    White = "White",
+    Black = "Black",
+}
+
