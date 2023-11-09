b0a9d702c54f4ba5d6c311b46b60024282c2f363
drespana <drespana@google.com>
2023-11-09 17:32:55 -0600

    new practice app

--- Functions.ts
+++
@@ -1,9 +0,0 @@
-
-// Union type and optionial parameters utilized in a function
-
-const add = (a:number, b:number, c?:number|string) => {
-    console.log(c)
-    return a+b
-}
-
-console.log(add(5,4,"peanut"));
